122e4562c952b2974eb2a635ef40a27b47b2868c
Mohamed-Ghanem155 <promopcbig458@gmail.com>
2026-09-01 00:30:07 +0300

    Use pdfx package for reliable PDF rendering on web

--- lib/widgets/pdf_viewer_web.dart
+++ lib/widgets/pdf_viewer_web.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:html' as html;
-import 'dart:ui' as ui;
+import 'package:pdfx/pdfx.dart';
 
 class PdfWebViewer extends StatefulWidget {
   final String assetPath;
@@ -12,51 +10,29 @@ class PdfWebViewer extends StatefulWidget {
 }
 
 class _PdfWebViewerState extends State<PdfWebViewer> {
-  String? _blobUrl;
-  final String viewId = 'pdf-iframe-\';
+  late PdfController _pdfController;
 
   @override
   void initState() {
     super.initState();
-    _loadPdf();
-  }
-
-  Future<void> _loadPdf() async {
-    try {
-      final ByteData data = await rootBundle.load(widget.assetPath);
-      final buffer = data.buffer.asUint8List();
-      final blob = html.Blob([buffer], 'application/pdf');
-      final url = html.Url.createObjectUrlFromBlob(blob);
-      setState(() {
-        _blobUrl = url;
-      });
-      // ignore: undefined_prefixed_name
-      ui.platformViewRegistry.registerViewFactory(
-        viewId,
-        (int id) => html.IFrameElement()
-          ..src = _blobUrl
-          ..style.border = 'none'
-          ..style.width = '100%'
-          ..style.height = '100%',
-      );
-    } catch (e) {
-      debugPrint('Error loading PDF: \');
-    }
+    _pdfController = PdfController(
+      document: PdfDocument.openAsset(widget.assetPath),
+    );
   }
 
   @override
   void dispose() {
-    if (_blobUrl != null) {
-      html.Url.revokeObjectUrl(_blobUrl!);
-    }
+    _pdfController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_blobUrl == null) {
-      return const Center(child: CircularProgressIndicator());
-    }
-    return HtmlElementView(viewType: viewId);
+    return PdfView(
+      controller: _pdfController,
+      backgroundDecoration: const BoxDecoration(
+        color: Colors.transparent,
+      ),
+    );
   }
 }
